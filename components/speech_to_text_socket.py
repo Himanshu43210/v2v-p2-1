ac40e8fd63bcb2c4d266e94235a483985f1610e5
Aashikhatri1 <134989740+Aashikhatri1@users.noreply.github.com>
2023-11-06 12:28:28 +0530

    a

--- components/speech_to_text_socket.py
+++ components/speech_to_text_socket.py
@@ -19,51 +19,6 @@ if not DEEPGRAM_API_KEY:
 # Define the Deepgram WebSocket URL with query parameters for transcription options
 DEEPGRAM_URL = f"wss://api.deepgram.com/v1/listen?punctuate=true&encoding=linear16&sample_rate=16000"
 
-# async def transcribe_socket(conn):
-#     # Set up the connection to Deepgram
-#     async with websockets.connect(
-#         DEEPGRAM_URL,
-#         extra_headers={"Authorization": f"Token {DEEPGRAM_API_KEY}"},
-#         ping_timeout=None,
-#     ) as dg_websocket:
-
-#         # Function to send data from the socket connection to Deepgram
-#         async def send_audio_from_socket():
-#             try:
-#                 while True:
-#                     data = conn.recv(1024)  # This buffer size can be changed depending on your use case
-#                     if not data:
-#                         break
-#                     await dg_websocket.send(data)
-#             except websockets.exceptions.ConnectionClosedOK:
-#                 pass  # Connection was closed cleanly
-#             finally:
-#                 # Tell Deepgram to process the last chunk of audio by sending an empty message
-#                 await dg_websocket.send(b'')
-
-#         # Function to receive transcriptions from Deepgram
-#         async def receive_transcriptions():
-#             transcript = ''
-#             try:
-#                 async for message in dg_websocket:
-#                     data = json.loads(message)
-#                     if data.get('is_final'):
-#                         # Extract the transcript from the received data
-#                         transcript = data['channel']['alternatives'][0]['transcript']
-#                         print("Transcript:", transcript)
-#                         break
-#             except websockets.exceptions.ConnectionClosedOK:
-#                 pass  # Connection was closed cleanly
-#             return transcript
-
-#         # Start sender and receiver coroutines
-#         sender_task = asyncio.create_task(send_audio_from_socket())
-#         receiver_task = asyncio.create_task(receive_transcriptions())
-
-#         # Wait for both tasks to complete
-#         await sender_task
-#         return await receiver_task
-
 async def transcribe_socket(conn):
     async with websockets.connect(
         DEEPGRAM_URL,
