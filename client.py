9a9d7fb9c0df43040ef721e14b8f8485c16b88c3
Aashikhatri1 <134989740+Aashikhatri1@users.noreply.github.com>
2023-11-06 21:07:44 +0530

    a

--- client.py
+++ client.py
@@ -1,4 +1,3 @@
-
 import socket
 import pyaudio
 import wave
@@ -14,84 +13,48 @@ PORT = 65432
 
 audio = pyaudio.PyAudio()
 
-stream = audio.open(format=FORMAT, channels=CHANNELS, rate=RATE, input=True, frames_per_buffer=CHUNK_SIZE)
+# stream = audio.open(format=FORMAT, channels=CHANNELS, rate=RATE, input=True, frames_per_buffer=CHUNK_SIZE)
 
-print("Recording...")
+# print("Recording...")
 
-frames = []
-for i in range(0, int(RATE / CHUNK_SIZE * RECORD_SECONDS)):
-    data = stream.read(CHUNK_SIZE, exception_on_overflow=False)
-    frames.append(data)
+# frames = []
+# for i in range(0, int(RATE / CHUNK_SIZE * RECORD_SECONDS)):
+#     data = stream.read(CHUNK_SIZE, exception_on_overflow=False)
+#     frames.append(data)
 
-print("Finished recording.")
+# print("Finished recording.")
 
-stream.stop_stream()
-stream.close()
+# stream.stop_stream()
+# stream.close()
 
+# Continuously record and send the audio in chunks
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-received_frames = []
+client_socket.connect((HOST, PORT))
+
+stream = audio.open(format=FORMAT, channels=CHANNELS, rate=RATE, input=True, frames_per_buffer=CHUNK_SIZE)
+print("Recording and streaming...")
 
 try:
-    client_socket.connect((HOST, PORT))
-
-    print("Sending data to server...")
-    for frame in frames:
-        client_socket.sendall(frame)
-
-    print("Finished sending data. Waiting for response...")
-    
-    data = client_socket.recv(CHUNK_SIZE)
-    while data:
-        received_frames.append(data)
-        data = client_socket.recv(CHUNK_SIZE)
-
-    print("Received response from server.")
-except Exception as e:
-    print(f"Error occurred: {e}")
+    while True:  # You can set a condition to end this loop
+        data = stream.read(CHUNK_SIZE, exception_on_overflow=False)
+        if not data:
+            break
+        client_socket.sendall(data)
+except KeyboardInterrupt:
+    # Handle any exception or break condition
+    print("Finished recording.")
 finally:
+    # Stop and close the audio stream
+    stream.stop_stream()
+    stream.close()
+    audio.terminate()
+
+    # Inform the server that the transmission is done
+    client_socket.shutdown(socket.SHUT_WR)
+
+    # Handle the server's response if needed
+    response = client_socket.recv(4096)  # Adjust buffer size if needed
+    print("Received:", response.decode())
+
+    # Close the socket connection
     client_socket.close()
-
-if received_frames:
-    print("Playing server's response...")
-    output_stream = audio.open(format=FORMAT, channels=CHANNELS, rate=RATE, output=True)
-
-    for frame in received_frames:
-        output_stream.write(frame)
-
-    output_stream.stop_stream()
-    output_stream.close()
-else:
-    print("No response received from server.")
-    
-response_audio_path = "server_response.wav"
-wf = wave.open(response_audio_path, 'wb')
-wf.setnchannels(CHANNELS)
-wf.setsampwidth(audio.get_sample_size(FORMAT))
-wf.setframerate(RATE)
-wf.writeframes(b''.join(received_frames))
-wf.close()
-
-print(f"Received audio saved to {response_audio_path}")
-
-# Then, play the server's response
-print("Playing server's response...")
-output_stream = audio.open(
-    format=FORMAT,
-    channels=CHANNELS,
-    rate=RATE,
-    output=True,
-)
-
-wf = wave.open(response_audio_path, 'rb')
-data = wf.readframes(CHUNK_SIZE)
-while data:
-    output_stream.write(data)
-    data = wf.readframes(CHUNK_SIZE)
-
-output_stream.stop_stream()
-output_stream.close()
-wf.close()
-
-print("Playback finished.")
-
-audio.terminate()
--- server.py
+++ server.py
@@ -1,3 +1,8 @@
+
+import socket
+import asyncio
+import websockets
+import json
 import socket
 import wave
 import pyaudio
@@ -11,6 +16,7 @@ import faiss
 import sys
 from sklearn.feature_extraction.text import TfidfVectorizer
 import sounddevice as sd
+import soundfile as sf
 import wavio
 import re
 from dotenv import load_dotenv
@@ -37,7 +43,6 @@ def generate_unique_id():
     return str(uuid.uuid4())
 
 conversation_id = generate_unique_id()
-
 # 1. FAISS Indexing
 # Convert phrases to embeddings for searching
 vectorizer = TfidfVectorizer().fit(phrases_dict.keys())
@@ -87,26 +92,6 @@ def handle_gpt_response(full_content):
             # Mark sentence as processed
             processed_sentences.add(sentence)
 
-# FAISS Indexing
-# Convert phrases to embeddings for searching
-vectorizer = TfidfVectorizer().fit(phrases_dict.keys())
-phrases_embeddings = vectorizer.transform(phrases_dict.keys())
-index = faiss.IndexIDMap(faiss.IndexFlatIP(phrases_embeddings.shape[1]))
-index.add_with_ids(
-    np.array(phrases_embeddings.toarray(), dtype=np.float32), 
-    np.array(list(range(len(phrases_dict))))
-)
-
-# Define the function to find matching audio
-def find_matching_audio(sentence):
-    sentence_embedding = vectorizer.transform([sentence]).toarray().astype(np.float32)
-    D, I = index.search(sentence_embedding, 1)
-    match_index = I[0][0]
-    matching_sentence = list(phrases_dict.keys())[match_index]
-    
-    if D[0][0] > 0.1:
-        return phrases_dict[matching_sentence]
-    return None
 
 # Define PyAudio parameters
 FORMAT = pyaudio.paInt16
@@ -114,10 +99,60 @@ CHANNELS = 1
 RATE = 44100
 CHUNK_SIZE = 1024
 p = pyaudio.PyAudio()
-        
-def chat_with_user(conn, addr):
-    print(f"Connected by {addr}")
 
+# Deepgram API credentials
+DEEPGRAM_API_KEY = os.getenv('DEEPGRAM_API_KEY')
+
+# Ensure the API key is available
+if not DEEPGRAM_API_KEY:
+    raise ValueError("The Deepgram API key is not set in the environment variables.")
+
+# Define the Deepgram WebSocket URL with query parameters for transcription options
+DEEPGRAM_URL = f"wss://api.deepgram.com/v1/listen?punctuate=true&encoding=linear16&sample_rate=44100"
+
+
+# Function to transcribe streaming audio
+async def transcribe_socket(conn):
+    async with websockets.connect(
+        DEEPGRAM_URL,
+        extra_headers={"Authorization": f"Token {DEEPGRAM_API_KEY}"},
+        ping_timeout=None,
+    ) as dg_websocket:
+
+        async def send_audio_from_socket():
+            while True:
+                data = conn.recv(4096)  # Increase buffer size for streaming
+                if not data:
+                    break
+                await dg_websocket.send(data)
+            await dg_websocket.send(b'')
+
+        async def receive_transcriptions():
+            full_transcript = ''
+            try:
+                async for message in dg_websocket:
+                    data = json.loads(message)
+                    if 'channel' in data:
+                        transcript = data['channel']['alternatives'][0]['transcript']
+                        if transcript:
+                            full_transcript += transcript
+                            print("Transcript:", transcript)
+                            handle_gpt_response(transcript)
+                        else:
+                            print("Received an empty transcript - the audio might not be recognized.")
+            except websockets.exceptions.ConnectionClosedOK:
+                pass  # Connection was closed cleanly
+            return full_transcript
+
+        sender_task = asyncio.create_task(send_audio_from_socket())
+        receiver_task = asyncio.create_task(receive_transcriptions())
+
+        await sender_task
+        return await receiver_task
+
+# Modify the chat_with_user function to work asynchronously
+async def chat_with_user(conn, addr):
+    print(f"Connected by {addr}")
     messages = [
         {
             "role": "system",
@@ -126,36 +161,20 @@ def chat_with_user(conn, addr):
             ),
         }
     ]
+    audio_path = "./assets/audio_files_pixel/Intro.wav"
+    data, samplerate = sf.read(audio_path)
+    sd.play(data, samplerate)    
+    sd.wait()
     processed_content = ""
     sentence_end_pattern = re.compile(r'(?<=[.?!])\s')
-    reception_window = 5  
+    
     try:
-        frames = []
-        start_time = time.time()
-        while True:
-            # Check if the time window has elapsed
-            if time.time() - start_time > reception_window:
-                print('Done receiving -- time window elapsed')
-                break
-            
-            conn.settimeout(reception_window - (time.time() - start_time))  # Update the socket timeout
-            
-            try:
-                data = conn.recv(CHUNK_SIZE)
-                if not data:
-                    print('Done receiving -- no more data')
-                    break
-                frames.append(data)
-                print(f"Received a chunk of data from {addr}")
-            except socket.timeout:
-                # If no data is received and the timeout occurs
-                print('Done receiving -- socket timeout')
-                break
 
-            
-            print('Done receiving')
-            # Process the received text
-            query = transcribe_socket(conn) 
+        while True:
+        # Start the transcription coroutine
+            query = await transcribe_socket(conn)
+            print(query)
+
             if query.lower() == "exit":
                 break
 
@@ -179,6 +198,7 @@ def chat_with_user(conn, addr):
                     new_content = content.replace(processed_content, "", 1).strip()  # Remove already processed content
                     elapsed_time = (datetime.datetime.now() - start_time).total_seconds()
                     print('gpt answer received: ', datetime.datetime.now())
+                    print(new_content)
 
                     # Split the content by sentence-ending punctuations
                     parts = sentence_end_pattern.split(new_content)
@@ -205,9 +225,6 @@ def chat_with_user(conn, addr):
             if content.strip():
                 messages.append({"role": "assistant", "content": content.strip()})
 
-            # Optionally, you can send back a confirmation or response
-            response = "Text received and processed."
-            conn.sendall(response.encode('utf-8'))
 
     except Exception as e:
         print(f"An error occurred: {e}")
@@ -215,9 +232,8 @@ def chat_with_user(conn, addr):
         conn.close()
         print(f"Disconnected {addr}")
 
-# Define server program
 # Modify the server_program function to handle async calls
-def server_program():
+async def server_program():
     host = '127.0.0.1'
     port = 65432
 
@@ -227,17 +243,10 @@ def server_program():
 
     print(f"Server listening on {host}:{port}")
 
-    loop = asyncio.new_event_loop()
-    asyncio.set_event_loop(loop)
-    try:
-        while True:
-            conn, addr = server_socket.accept()
-            loop.run_until_complete(chat_with_user(conn, addr))
-    except KeyboardInterrupt:
-        print("Server is closing...")
-    finally:
-        server_socket.close()
-        loop.close()
+    while True:
+        conn, addr = server_socket.accept()
+        await chat_with_user(conn, addr)
 
+# Modify the entry point to run the async server program
 if __name__ == '__main__':
-    server_program()
+    asyncio.run(server_program())
